abc0ce3065fc1dc1707ef682dfb8925ed0ff7349
Gladys Maniago <gfmaniago@gmail.com>
2016-01-13 15:48:03 -0800

    Max between 2 numbers

---
+++ exercise1.js
@@ -0,0 +1,17 @@
+// Define a function max() that takes two numbers as arguments and 
+// returns the largest of them. Use the if-then-else construct available 
+// in Javascript.
+
+function max(a, b){
+	if(a < b){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
+// The function takes two parameters, if the first parameters
+// has a greater value than the second parameter then this 
+// function will return the first parameter. If the second parameter has
+// a great value than the first, this is function will return the second parameter.
