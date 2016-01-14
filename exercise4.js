50278cf21cbbafae0bc5e81d6bcea32c3089af01
Gladys Maniago <gfmaniago@gmail.com>
2016-01-13 20:23:42 -0800

    Grade Assigner

---
+++ exercise4.js
@@ -0,0 +1,21 @@
+// Write a function named assignGrade that:
+// takes 1 argument, a number score.
+// returns a grade for the score, either "A", "B", "C", "D", or "F".
+// Call that function for a few different scores and log the result to make sure it works.
+
+function assignGrade(score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 80) {
+        return 'B';
+    } else if (score > 70) {
+        return 'C';
+    } else if (score > 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+console.log('You have a ' + assignGrade(92) + ' in class.');
+console.log('You have a ' + assignGrade(74) + ' in class.');
